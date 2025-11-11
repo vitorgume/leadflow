b94a20359be5cc514682279299391f60c828d508
vitorgume <vitorhvvieira@gmail.com>
2025-11-11 17:11:11 -0300

    Corrigindo erros no terraform

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,8 +1,17 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5"
+    }
   }
+
+  backend "s3" {}
 }
 
 provider "aws" {
