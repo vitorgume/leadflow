888d74ccff599226a3c92d97bb2e05ba5e76349c
vitorgume <vitorhvvieira@gmail.com>
2025-11-11 08:13:11 -0300

    Correção na mensagem de inativo grau 1

--- services/api-principal/src/main/java/com/guminteligencia/ura_chatbot_ia/application/usecase/mensagem/mensagens/MensagemRecontatoIantivoG1.java
+++ services/api-principal/src/main/java/com/guminteligencia/ura_chatbot_ia/application/usecase/mensagem/mensagens/MensagemRecontatoIantivoG1.java
@@ -9,22 +9,29 @@ public class MensagemRecontatoIantivoG1 implements MensagemType {
 
     @Override
     public String getMensagem(String nomeVendedor, Cliente cliente) {
+
         StringBuilder mensagem = new StringBuilder();
 
-        String nomeUsuario = cliente.getNome() == null ? "Paciente" : cliente.getNome();
+        if(cliente.getNome() == null && cliente.getDorDesejoPaciente() == null) {
+            mensagem.append("Olá, tudo bem? 😊").append("\n");
+            mensagem.append("Sou a Luiza, assistente do Dr. Felipe.").append("\n");
+            mensagem.append("Notei que paramos nossa conversa. Para darmos sequência à sua triagem, preciso apenas que você me diga seu nome e o procedimento que te interessa.")
+                    .append("\n");
+            mensagem.append("Assim, garantimos que você não perca tempo para iniciar o atendimento.");
 
-        mensagem.append(nomeUsuario)
-                .append(" notei que paramos nossa conversa e quis retornar para que você não perca o seu lugar na fila de agendamento.")
-                .append("\n");
+        } else {
+            mensagem.append(cliente.getNome())
+                    .append(" notei que paramos nossa conversa e quis retornar para que você não perca o seu lugar na fila de agendamento.")
+                    .append("\n");
 
-        mensagem.append("O seu objetivo (")
-                .append(cliente.getDorDesejoPaciente())
-                .append(") já estava quase sendo encaminhado para a avaliação de 1 hora com o Dr. Felipe.")
-                .append("\n");
+            mensagem.append("O seu objetivo (")
+                    .append(cliente.getDorDesejoPaciente())
+                    .append(") já estava quase sendo encaminhado para a avaliação de 1 hora com o Dr. Felipe.")
+                    .append("\n");
 
-        mensagem.append("Podemos dar sequência agora? A agenda do Dr. Felipe está com poucos horários disponíveis, e gostaria de garantir que você de início ao seu tratamento! 😊");
-
-
+            mensagem.append("Podemos dar sequência agora? A agenda do Dr. Felipe está com poucos horários disponíveis, e gostaria de garantir que você de início ao seu tratamento! 😊");
+        }
+        
         return mensagem.toString();
     }
 
